7afceb5bc500db094c652a63d460a1bc08dfa0c9
Zaky97 <zaky7645@gmail.com>
2024-02-10 21:57:50 +0700

    Co-authored-by: update 5 files Diazz13 <Diazz13@users.noreply.github.com>

--- main/script.js
+++ main/script.js
@@ -1,5 +1,25 @@
-// Optional: Auto-resize wave height with window resize
-window.addEventListener("resize", () => {
-  const wave = document.querySelector(".wave");
-  wave.style.height = `${window.innerHeight}px`;
-});
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiA-CPBjHgdNMSkECYJ3D55S-OJWeRpNM",
+  authDomain: "noir-b0cf8.firebaseapp.com",
+  databaseURL:
+    "https://noir-b0cf8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "noir-b0cf8",
+  storageBucket: "noir-b0cf8.appspot.com",
+  messagingSenderId: "265741421264",
+  appId: "1:265741421264:web:5d6af087a996b58a9cecd5",
+  measurementId: "G-CDZKCSXS56",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
